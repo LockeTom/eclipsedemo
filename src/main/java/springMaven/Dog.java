704f6545f7270f17ec545da273f173a39bd762b1
LockeTom <zcgs4321@outlook.com>
2019-01-13 15:21:42 +0800

    添加了一个Dog类

---
+++ src/main/java/springMaven/Dog.java
@@ -0,0 +1,19 @@
+package springMaven;
+
+public class Dog {
+private String dogName;
+private String dogGender;
+public String getDogName() {
+	return dogName;
+}
+public void setDogName(String dogName) {
+	this.dogName = dogName;
+}
+public String getDogGender() {
+	return dogGender;
+}
+public void setDogGender(String dogGender) {
+	this.dogGender = dogGender;
+}
+
+}
